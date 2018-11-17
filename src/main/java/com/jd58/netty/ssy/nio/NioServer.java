3b516b7e094385c97cf3df7c8307833d221e03aa
yanghuitao <yanghuitao@jd.com>
2018-11-17 23:21:11 +0800

    nio-netty

---
+++ src/main/java/com/jd58/netty/ssy/nio/NioServer.java
@@ -0,0 +1,24 @@
+package com.jd58.netty.ssy.nio;
+
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
+/**
+ * Created by yht on 2018/11/17.
+ */
+public class NioServer {
+
+    public static void main(String[] args) throws Exception{
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+        ServerSocket serverSocket = serverSocketChannel.socket();
+        serverSocket.bind(new InetSocketAddress(8899));
+
+        Selector selector = Selector.open();
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+    }
+}
